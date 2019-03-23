08999f4b21d698ce4f5a245a7f4e8595bf4c06e0
MIkkel Sang Mee Baunsgaard <baunsgaard38@gmail.com>
2019-03-23 18:56:09 +0100

    Auto stash before merge of "master" and "origin/master"

--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -11,6 +11,9 @@ public class playerScript : MonoBehaviour
     public float jumpF;
     private bool jumpR = false;
     public int points;
+    public int direction;
+    private bool shootR = false;
+    public GameObject bullets;
 
     // Use this for initialization
     void Start()
@@ -20,15 +23,37 @@ public class playerScript : MonoBehaviour
 
     void FixedUpdate()
     {
+        if(!Mathf.Approximately(Input.GetAxisRaw("Horizontal"), 0))
+        {
+            direction = (int)Input.GetAxisRaw("Horizontal");
+        }
+
         rbplayer.AddForce(new Vector2(Input.GetAxisRaw("Horizontal") * speed, 0));
-        if(Mathf.Approximately(Input.GetAxisRaw("Vertical"), 1)){
+        if(Mathf.Approximately(Input.GetAxisRaw("Jump"), 1)){
             if(jumpR == false){
                 jumpR = true;
-                rbplayer.AddForce(new Vector2(0, (col.IsTouchingLayers(LayerMask.GetMask("Floor")) ? 1 : 0) * jumpF * Input.GetAxisRaw("Vertical")));
+                rbplayer.AddForce(new Vector2(0, (col.IsTouchingLayers(LayerMask.GetMask("Floor")) ? 1 : 0) * jumpF * Input.GetAxisRaw("Jump")));
             }
         }
-        if(Mathf.Approximately(Input.GetAxisRaw("Vertical"), 0)){
+        if(Mathf.Approximately(Input.GetAxisRaw("Jump"), 0)){
             jumpR = false;
         }
+
+
+        if (Mathf.Approximately(Input.GetAxisRaw("Shoot"), 1))
+        {
+            if (shootR == false)
+            {
+                shootR = true;
+                GameObject bullet = Instantiate(bullets);
+                bullet.transform.position = this.gameObject.transform.position;
+                bullet.gameObject.GetComponent<projectile>().direction = direction;
+            }
+        }
+        if (Mathf.Approximately(Input.GetAxisRaw("Shoot"), 0))
+        {
+            shootR  = false;
+        }
+
     }
 }
---
+++ Assets/Scripts/projectile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile : MonoBehaviour {
+
+    public float speed;
+    public Rigidbody2D rgBody;
+    public GameObject player;
+    public int direction;
+
+	// Use this for initialization
+	void Start () {
+        rgBody.AddForce(new Vector2(speed * direction, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
