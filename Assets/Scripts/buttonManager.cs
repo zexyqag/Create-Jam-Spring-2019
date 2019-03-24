c1cc6a56018f894420be3e6fbfb734cd2c2bac7d
Marcus <m.h.otterstreom@gmail.com>
2019-03-24 01:04:39 +0100

    GLHF
    + addde Main menu

---
+++ Assets/Scripts/buttonManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonManager: MonoBehaviour
+{
+    public void newGameBtn (string newGameLevel)
+    {
+        SceneManager.LoadScene(newGameLevel);
+    }
+
+    public void exitGameBtn()
+    {
+        Application.Quit();
+    }
+}
